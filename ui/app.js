70169edaa592e7131ef575650504ed19ce90f5ad
ben_kung <ben_kung@weleader.com.tw>
2020-09-07 13:54:27 +0800

    ddd

--- ui/app.js
+++ ui/app.js
@@ -1,11 +1,15 @@
-
+import {isLogin} from  './helpers/ahtuHelper.js'
 import "./components/site.js";
 import { router } from "./routes.js";
 ((document) => {
 
   new Vue({
-    router
-  }).$mount('#app');
+    el: '#app',
+    router,
+    created:function(){
+      console.log(isLogin())
+    }
+  })//.$mount('#app');
   feather.replace();
 
 })(document);
