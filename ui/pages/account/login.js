c542c531e7193994c1a6cf2aefe32c517ce8ef10
ben_kung <ben_kung@weleader.com.tw>
2020-09-07 16:55:18 +0800

    OK

--- ui/pages/account/login.js
+++ ui/pages/account/login.js
@@ -9,11 +9,23 @@ export const loginPageComponent = Vue.component('login-page', {
      }
     },
     template: `
-    <div> 
-        <h1>登入</h1>
-        <button @click='go'>fucking go</button>
-        </div> 
-
-`
+        <div> 
+        <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="button" class="btn btn-primary"  @click='go'>Submit</button>
+      </form>
+        </div> `
   }) 
 export   default loginPageComponent  
